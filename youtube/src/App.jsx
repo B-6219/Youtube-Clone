e1c9d3f6574a02655d433ec42c9b89e543042297
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-08-06 13:49:09 +0100

    kickoff

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,9 +1,17 @@
+import NavBar from "./components/NavBar"
 
 const App = () => {
   return(
     <>
-    <h1 className="text-3x1">Hi there</h1>
+    <section className="bg-[#000000] h-auto">
+      <NavBar/>
+      <h1 className="text-3x1 font-serif text-center">Hi there</h1>
+
+    </section>
+
     </>
+
+    
   )
 }
 
---
+++ youtube/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import menu from '../assets/images'
+
+const NavBar = () => {
+  return (
+		<div className=''>
+			<img src= {menu} alt="" />
+			<p className='text-white'>VidTube</p>
+		</div>
+  )
+}
+
+export default NavBar
