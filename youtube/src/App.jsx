3eab90ce1f406da63a836677c836638b22b3f664
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-08-09 12:30:35 +0100

    sidebar issues

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,16 +1,18 @@
 import NavBar from "./components/NavBar"
-import SideBar from "./components/SideBar.jsx"
+import SideBar from "./components/SideBar"
 
 
 const App = () => {
   return(
     <>
+
     <section className="bg-[#000000] h-screen">
       <NavBar/>
 
       <div className="flex flex-1">
         <Sidebar/>
       </div>
+
 
       <h1 className="text-3x1 text-white font-serif text-center">Hi there</h1>
       
