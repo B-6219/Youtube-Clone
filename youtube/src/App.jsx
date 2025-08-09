5bf6a7f387ec27194eb9e696f23e5b6cf47fd334
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-08-09 12:22:49 +0100

    Sidebar issues😭

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,13 +1,21 @@
 import NavBar from "./components/NavBar"
-import {assets} from  './assets/images/vidTube_img/images/assets.js'
+import SideBar from "./components/SideBar.jsx"
+
+
 const App = () => {
   return(
     <>
     <section className="bg-[#000000] h-screen">
       <NavBar/>
 
-      <h1 className="text-3x1 font-serif text-center">Hi there</h1>
+      <div className="flex flex-1">
+        <Sidebar/>
+      </div>
+
+      <h1 className="text-3x1 text-white font-serif text-center">Hi there</h1>
+      
     </section>
+    
 
     </>
 
--- youtube/src/components/NavBar.jsx
+++ youtube/src/components/NavBar.jsx
@@ -3,20 +3,36 @@ import { assets } from '../assets/images/vidTube_img/images/assets'
 
 const NavBar = () => {
   return (
-		<div className='flex'>
-			<div className='flex gap-4 h-9'>
-				<img src={assets.menu} alt="" className='text-4xl'/>
-				<img src={assets.logo} alt="" className='text-2xl'/>
-			</div>
-
-			<div className='items-center flex gap-8'>
-				<input type="text" placeholder='Seach for anything' className='bg-transparent rounded-sm border-inherit placeholder:text-gray-50'/>
-				<img src={assets.search} alt="" />
-				<img src={assets.voice_search} alt="" className='size-9'/>
-			</div>
-
+  <> 
+	<div className="flex w-full items-center justify-between p-4 bg-gray-900">
+	 {/* Left */}
+		<div className="flex gap-4 items-center">
+			<img src={assets.menu} alt="" className="w-9 h-9" />
+			<img src={assets.logo} alt="" className="w-16" />
 		</div>
 
+	 {/* Middle */}
+		<div className="flex items-center gap-2 bg-gray-800 rounded px-3 py-1">
+				<input
+					type="text"
+					placeholder="Search for anything"
+					className="bg-transparent outline-none placeholder:text-gray-400 h-7"
+				/>
+				<img src={assets.search} alt="" className="w-6 h-6" />
+				<img src={assets.voice_search} alt="" className="w-9 h-7" />
+		</div>
+
+		<div className='flex items-center gap-4'>
+			<img className='h-7' src={assets.upload} alt="" />
+			<img className='h-7' src={assets.notification} alt="" />
+			<img className='h-7' src={assets.more} alt="" />
+			<img className='rounded-full h-7' src={assets.jack} alt="" />
+		</div>
+	</div>
+
+
+
+</>
   )
 }
 
